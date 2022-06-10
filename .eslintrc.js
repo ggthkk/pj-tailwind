a2bb76fe01981a0a77c39afedabe81da8a7f809b
dawos78 <minimojo78@hotmail.com>
2022-06-10 09:54:00 +0700

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,31 @@
 module.exports = {
   root: true,
+
   extends: [
     "plugin:vue/essential",
     "plugin:prettier/recommended",
     "eslint:recommended"
   ],
+
+  env: {
+    node: true
+  },
+
+  'extends': [
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    '@vue/prettier'
+  ],
+
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import test from "../views/test.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -11,13 +11,17 @@ const routes = [
     component: Home,
   },
   {
+    path: "/",
+    name: "test",
+    component: test,
+  },
+  {
     path: "/about",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
