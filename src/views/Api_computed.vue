0d033ad45b44208ecaa2b2b616cab9102202c30d
ggthkk <babagatom1@gmail.com>
2022-06-17 17:10:44 +0700

    update 17:00

--- src/views/Api_computed.vue
+++ src/views/Api_computed.vue
@@ -2,9 +2,14 @@
     <div class="grid place-items-center h-auto">
       <div class="w-9/12 m-5 rounded overflow-hidden shadow-lg">
         <div class="px-2 py-4">
-          <div class="font-bold text-2xl mb-4">Api option Data</div>
+          <div class="font-bold text-2xl mb-4">Api option Computed</div>
           <p class="text-gray-700 text-base">
-            awdaw
+            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Input1</label>
+            <input type="number" v-model="a" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+<br>
+            <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Input2</label>
+            <input type="number" v-model="b" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+          <label for="base-input" class="block mt-3 text-md">value is: {{ sum }}</label>
           </p>
         </div>
       </div>
@@ -14,11 +19,17 @@
 <script>
 export default {
   data() {
-    return {
-      value: ''
+    return{
+      a:'',
+      b:'',
     }
   },
-};
+  computed: {
+    sum() {
+      return parseInt(this.a) + parseInt(this.b)
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/Api_methods.vue
+++ src/views/Api_methods.vue
@@ -1,22 +1,56 @@
 <template>
-    <div class="grid place-items-center h-auto">
-      <div class="w-9/12 m-5 rounded overflow-hidden shadow-lg">
-        <div class="px-2 py-4">
-          <div class="font-bold text-2xl mb-4">Api option Data</div>
-          <p class="text-gray-700 text-base">
-            awdawawdawdawawd
-          </p>
-        </div>
+  <div class="grid place-items-center h-auto">
+    <div class="w-9/12 m-5 rounded overflow-hidden shadow-lg">
+      <div class="px-2 py-4">
+        <div class="font-bold text-2xl mb-4">Api option Methods</div>
+        <p class="text-gray-700 text-base">
+          <label
+            for="base-input"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Name</label
+          >
+          <input
+            type="text" v-model="a"
+            class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          />
+          <br />
+          <label
+            for="base-input"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Lastname</label
+          >
+          <input
+            type="text" v-model="b"
+            class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          />
+          <label for="base-input" class="block mt-3 text-lg"
+            >Fullname is: {{ getfullname }}</label
+          >
+          <br />
+          <button
+            class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+          @click="sum()">
+            Button
+          </button>
+        </p>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      value: ''
-    }
+      a: '',
+      b: '',
+      getfullname:''
+    };
+  },
+  methods: {
+    sum() {
+      return this.getfullname = this.a + " " + this.b
+    },
   },
 };
 </script>
